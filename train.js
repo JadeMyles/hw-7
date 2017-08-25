0446baaf9ff9d0148445c9b7f1dc55b07a40927f
JadeMyles <jade.myles@yahoo.com>
2017-08-25 10:23:16 -0500

    Add files via upload

---
+++ train.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyBlZxWud9H6jarL3uYzYN5oYrFLKaQol-E",
+    authDomain: "trainstrainstrains-f487e.firebaseapp.com",
+    databaseURL: "https://trainstrainstrains-f487e.firebaseio.com",
+    projectId: "trainstrainstrains-f487e",
+    storageBucket: "",
+    messagingSenderId: "906035998792"
+  };
+  firebase.initializeApp(config);
